57d308f380aed45971935c5f6c3d0d49968166f9
User <user@example.com>
2026-05-22 20:46:44 +0800

    fix: 升级到Node 24并强制Vercel运行时版本

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,12 +1,11 @@
 import { defineConfig } from 'astro/config';
 import tailwind from '@astrojs/tailwind';
-import vercel from '@astrojs/vercel/serverless';
+import vercel from '@astrojs/vercel';
 
 export default defineConfig({
   integrations: [tailwind()],
   output: 'server',
   adapter: vercel({
-    webAnalytics: { enabled: true },
-    nodeVersion: 20,
-  })
+    nodeVersion: 24,
+  }),
 });
